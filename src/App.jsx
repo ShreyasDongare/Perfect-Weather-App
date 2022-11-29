b508962d72a746927c40cc6ce44a152089b0d674
Shreyas Dongare <dongareshreyas@gmail.com>
2022-11-29 10:01:09 +0530

    fixing bug in try and catch block

--- src/App.jsx
+++ src/App.jsx
@@ -21,20 +21,19 @@ import ErrorMessage from "./components/ErrorMessage";
 const API_KEY = "a760ebe2645772b675936874aa6dedb0";
 
 const App = () => {
-  const [location, setLocaltion] = useState("Pune");
+  const [location, setLocaltion] = useState("paris");
   const [isError, setIsError] = useState(false)
   const [data, setData] = useState(null);
+  const [isHigh, setIsHigh] = useState(true) 
 
-  const handleSubmit = (city) => {
-    console.log(city);
-    setLocaltion(city);
-  };
-
+ 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
 
    //fetching data
+   //fetching weather Data data from costum hook by passing URL
    const fetchData = async () => {
      try {
+      setIsError(false)
        const resp = await fetch(url);
        const respJson = await resp.json();
        console.log(respJson)
@@ -43,6 +42,7 @@ const App = () => {
        }else{
         setIsError(true)
        }
+       
      } catch (error) {
        setIsError(true);
        console.log(error);
@@ -53,20 +53,12 @@ const App = () => {
      fetchData();
    }, [url]);
 
-  //fetching weather Data data from costum hook by passing URL
- 
+ //handle Submit of Form Component
+ const handleSubmit = (city) => {
+   console.log(city);
+   setLocaltion(city);
+ };
 
-  // if(isError){
-  //   console.log("error")
-  //   return <div>
-  //    <h1>Enter Right location</h1>
-  //   </div>
-  // }
-
-
-  //  const {name,wind:{speed}, main:{temp, feels_like, humidity},sys:{country} } = data
-
-  //  console.log(name, speed );
 
   //setting laoder if data is falsy or loading
   if (!data) {
@@ -106,15 +98,30 @@ const App = () => {
       break;
   }
 
+let temp = Math.floor(data.main.temp)
+
+// if(temp < 25){
+//   setIsHigh(true)
+// }
+// else{
+//   setIsHigh(false)
+// }
+  
+//   console.log(isHigh)
   return (
     <div className="w-full h-screen px-8 flex flex-col items-center justify-center">
-      <div className="max-w-screen-sm h-[700px] py-6 px-8 w-full bg-gradient-to-br  from-teal-400  to-cyan-700 rounded shadow-xl shadow-gray-500">
+      <div
+        className={`max-w-screen-sm h-[600px] py-6 px-8 w-full bg-gradient-to-br 
+        ${
+          isHigh ? " from-teal-400 to-cyan-700" : " from-orange-400 to-red-500"
+        } rounded shadow-xl shadow-gray-500`}
+      >
         <div className="w-full text-center my-4 ">
           <h1 className="text-3xl font-medium text-white">Perfect Weather</h1>
         </div>
         <Form handleSubmit={handleSubmit} />
         {isError ? (
-          <ErrorMessage/>
+          <ErrorMessage />
         ) : (
           <>
             <WeatherTop icon={icon} data={data} />
